bc062e04b06f52fe7ba9421e72cb280088c79813
rising <625687551@qq.com>
2017-01-04 16:37:04 +0800

    添加IP代理以及UA的部分-

--- dingdian/middlewares/RandomProxy.py
+++ dingdian/middlewares/RandomProxy.py
@@ -2,15 +2,17 @@ import random
 import pandas as pd
 from sqlalchemy import create_engine
 from dingdian import settings
-from jiandan.db.db_helper import DB_Helper##这里要好好修改
 
 '''
 这个类主要产生随机的IP
 '''
 class RandomProxy(object):
-    def __init__(self):
-        self.db_helper=DB_helper()
-        self.count=self.db_helper.proxy.count()
+    def __init__(self):#初始化IP池
+        dbfile = r'/home/rising/Program/IPProxyPool/IPProxyPool_py3/data/proxy.db'
+        db_conn = create_engine('sqlite:///' + dbfile)
+        df = pd.read_sql('select * from proxys', db_conn)
+        df.update(df.port.map(str))
+        self.iplist = list('http://' + df.ip + ':' + df.port)
     def process_request(self,request,spider):
         '''
         在请求上添加代理
@@ -18,11 +20,7 @@ class RandomProxy(object):
         :param spider
         :return
         '''
-        dbfile = r'/home/rising/Program/IPProxyPool/IPProxyPool_py3/data/proxy.db'
-        db_conn = create_engine('sqlite:///' + dbfile)
-        df = pd.read_sql('select * from proxys', db_conn)
-        df.update(df.port.map(str))
-        iplist = list('http://' + df.ip + ':' + df.port)
-        ip = random.choice(iplist)
+
+        ip = random.choice(self.iplist)
         # result = self.db_helper.findOneResult({'proxyId':ip})
         request.meta['proxy'] =ip
--- dingdian/middlewares/RandomUserAgent.py
+++ dingdian/middlewares/RandomUserAgent.py
@@ -1,15 +1,16 @@
 import random
+from dingdian.settings import USER_AGENTS
 
 class RandomUserAgent(object):
     '''
     这个类主要随机产生UA
     '''
     def __init__(self,agents):
-        self.agents=agents
+        self.agents=USER_AGENTS
 
     @classmethod
     def from_crawler(cls,crawler):
         return cls(crawler.setting.getlist('USER_AGENTS'))
 
     def process_request(self,request,spider):
-        request.headers.setdault('User-Agent',random.choice(self,agents))
+        request.headers.setdault('User-Agent' , random.choice(self.agents))

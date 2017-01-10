e79c08b41c5296f2ec232f36c8e205815ff6d41d
rising <625687551@qq.com>
2017-01-10 11:22:42 +0800

    添加IP代理以及UA的部分-

--- dingdian/middlewares/RandomUserAgent.py
+++ dingdian/middlewares/RandomUserAgent.py
@@ -5,12 +5,12 @@ class RandomUserAgent(object):
     '''
     这个类主要随机产生UA
     '''
-    def __init__(self,agents):
+    def __init__(self):
         self.agents=USER_AGENTS
 
-    @classmethod
-    def from_crawler(cls,crawler):
-        return cls(crawler.setting.getlist('USER_AGENTS'))
+    # @classmethod
+    # def from_crawler(cls,crawler):
+    #     return cls(crawler.setting.getlist('USER_AGENTS'))
 
     def process_request(self,request,spider):
-        request.headers.setdault('User-Agent' , random.choice(self.agents))
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
